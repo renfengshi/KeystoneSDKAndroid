99faa1f8da2bf11a40cb56e92084c6cc52568ab1
Renfeng Shi <renfeng@keyst.one>
2023-03-09 17:05:16 +0800

    KeystoneSDK

---
+++ library/src/main/kotlin/com/keystone/sdk/KeystoneSDK.kt
@@ -0,0 +1,19 @@
+package com.keystone.sdk
+
+import com.sparrowwallet.hummingbird.UR
+import com.sparrowwallet.hummingbird.UREncoder
+
+class KeystoneSDK {
+    external fun parseSolSignature(cbor: String): String
+
+    public fun encodeQR(type: String, cbor: String, maxFragmentLen: Int = 100, minFragmentLen: Int = 10): UREncoder {
+        val ur = UR.fromBytes(type, cbor.toByteArray());
+        return UREncoder(ur, maxFragmentLen, minFragmentLen, 0)
+    }
+
+    companion object {
+        init {
+            System.loadLibrary("ur_registry_ffi")
+        }
+    }
+}
