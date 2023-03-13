cf7ad51afc02a1e7dc0c55c9e6ba6d1cc5a82b00
Renfeng Shi <renfeng@keyst.one>
2023-03-13 11:20:13 +0800

    test

--- library/env.sh
+++ library/env.sh
@@ -6,15 +6,10 @@
 #export RUST_ANDROID_GRADLE_CARGO_COMMAND=$HOME/.cargo/bin/cargo # set the path on your computer
 #export RUST_ANDROID_GRADLE_RUSTC_COMMAND=$HOME/.cargo/bin/rustc # set the path on your computer
 
-rustc -V
-python -V
-
 if command -v rustc >/dev/null 2>&1; then
   echo 'rustc exists'
 else
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
   source "$HOME/.cargo/env"
   rustc -V
-  export RUST_ANDROID_GRADLE_CARGO_COMMAND=$HOME/.cargo/bin/cargo # set the path on your computer
-  export RUST_ANDROID_GRADLE_RUSTC_COMMAND=$HOME/.cargo/bin/rustc # set the path on your computer
 fi
